846b447cfb31fc8af52774de9d65fc734bd5c62a
krawler <krawler@klinux.(none)>
2012-08-13 17:04:59 +0530

    Clean up

--- mavenHrmsSpring/src/main/utils/DB Changes/HRMS.3.0.sql
+++
@@ -1,3 +0,0 @@
-alter table costcenter modify column name varchar(100);
-
-
--- mavenHrmsSpring/src/main/utils/DB Changes/HRMS.3.8.sql
+++
@@ -1,6 +0,0 @@
-Add following line in web.xml after context-param of hrmsURL:
-
-<context-param>
-	<param-name>eleaveURL</param-name>
-    <param-value>http://192.168.0.198:8080/eleave/</param-value>
-</context-param>
